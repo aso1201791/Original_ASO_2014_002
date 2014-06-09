a3598df93a5f08eff4ed00cd31d7c41412b5a988
student <student@PC111S42.fgakusei.local>
2014-06-09 15:21:14 +0900

    追加1

---
+++ src/com/example/originalaso_2014_002/HitokotoActivity.java
@@ -0,0 +1,15 @@
+package com.example.originalaso_2014_002;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HitokotoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.hitokoto_activity);
+	}
+
+}
---
+++ src/com/example/originalaso_2014_002/MaintenanceActivity.java
@@ -0,0 +1,15 @@
+package com.example.originalaso_2014_002;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MaintenanceActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.maintenance_activity);
+	}
+
+}
