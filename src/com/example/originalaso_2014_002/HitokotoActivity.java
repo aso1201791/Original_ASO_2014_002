603c3f75796850463dbae20ed1eb0bc6df525b93
student <student@PC111S42.fgakusei.local>
2014-06-11 11:17:54 +0900

    編集2

--- src/com/example/originalaso_2014_002/HitokotoActivity.java
+++ src/com/example/originalaso_2014_002/HitokotoActivity.java
@@ -2,14 +2,21 @@ package com.example.originalaso_2014_002;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
-public class HitokotoActivity extends Activity {
+public class HitokotoActivity extends Activity implements View.OnClickListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.hitokoto_activity);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+		
 	}
 
 }
