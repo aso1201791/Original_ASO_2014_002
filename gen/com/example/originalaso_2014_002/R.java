c0aa18738e8812fc50c80cf46de818b989d3eda9
student <student@PC111S42.fgakusei.local>
2014-06-16 11:16:38 +0900

    編集4

--- gen/com/example/originalaso_2014_002/R.java
+++ gen/com/example/originalaso_2014_002/R.java
@@ -22,15 +22,17 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int robin=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
+        public static final int action_settings=0x7f08000a;
         public static final int check=0x7f080005;
         public static final int editText1=0x7f080002;
-        public static final int listView1=0x7f080006;
+        public static final int imageView1=0x7f080006;
+        public static final int listView1=0x7f080007;
         public static final int mente=0x7f080003;
-        public static final int modoru=0x7f080007;
-        public static final int sakujo=0x7f080008;
+        public static final int modoru=0x7f080008;
+        public static final int sakujo=0x7f080009;
         public static final int textView1=0x7f080000;
         public static final int textView2=0x7f080001;
         public static final int toroku=0x7f080004;
---
+++ src/com/example/originalaso_2014_002/MySQLiteOpenHelper.java
@@ -0,0 +1,17 @@
+package com.example.originalaso_2014_002;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MySQLiteOpenHelper extends SQLiteOpenHelper {
+
+	/**
+	 * @param context 呼び出しコンテクスト
+	 * @param name 利用DB名
+	 * @param factory カーソルファクトリー
+	 * @param version DBバージョン
+	 */
+	public MySQLiteOpenHelper (Context context){
+
+	}
+}
